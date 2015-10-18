4193375a6f7d43617085b9c403f23ba81a6719e4
Hannah Gamiel <hannah.gamiel@gmail.com>
2015-10-18 15:07:24 -0700

    Moved a lot of variables for global access, setting up script to eventually handle 140 char twitter functionality (still need to implement hard limit check on character limit), added "d'ni" to list of proper nouns, took away literals and replaced them with variables so they can be changed more easily in the future

--- generator.py
+++ generator.py
@@ -8,23 +8,42 @@ f_output = open('output.txt','w')
 f_feed = open('atrus.txt','r')
 r_feed = f_feed.read()
 
-punc = ["?", "--", ".", ",", "!", " ", ";", ".", " "]
-sentences = list(re.split(r' *[\.\?!][\'"\)\]]* *', r_feed))
-names = ["catherine", "atrus", "achenar", "sirrus"]
+punc = ["?", "--", ".", ",", "!", " ", ";", ".", " "] #list of puncation
+puncRequireCap = ["?", ".", "!", ":", ";"] #list of punctuation that requires capitalized letter after
+sentences = list(re.split(r' *[\.\?!][\'"\)\]]* *', r_feed)) #list of sentences
+pNouns = ["catherine", "atrus", "achenar", "sirrus", "d'ni"] #list of names/proper nouns
 
+oBuffer = []
+oString = ""
+sig = "\n#PoemsFromAtrus"
+
+n = 5 #num sentences
+rm = 1 #random modifier
+minwps = 3 #minimum words per sentence
+minwpl = 5 #minimum words per line
+maxwpl = 8 #maximum words per line
 lc = 1 #last cap bool
 lastIndex = 0 #last sentence index
+clim = 140 - len(sig) #twitter character limit
+uclim = 1 #whether or not to character limit
 
-def writeAndPrint(line):
-	f_output.write(line + "\n")
-	print(line)
+def addToOutput(segment):
+	f_output.write(segment + "\n")
+	oBuffer.append(segment)
 
-def createSentence(freeverse):
+def prepareOutput():
+	global oString
+	oString = "\"" + "\n".join(oBuffer) + "\"" + "\n" + sig
+
+def printOutput():
+	print(oString)
+
+def createSentence():
 	global lastIndex
 	s = []
 	newIndex = randint(0, len(sentences)-1)
 
-	if(freeverse):
+	if(rm):
 		while((newIndex == lastIndex) or (newIndex + 1 == lastIndex) or (newIndex - 1 == lastIndex)):
 			newIndex = randint(0, len(sentences)-1)
 		lastIndex = newIndex
@@ -42,54 +61,61 @@ def modifyFirstChar(verse):
 	ofl = ofw[0] #original first letter of verse
 	iofw =  ofw[1:len(verse[0])] #original first word minus first letter
 
-	if (lc == 1 or (ofl == "i" and len(ofw) == 1) or ofw.lower() in names):
+	if (lc == 1 or (ofl == "i" and len(ofw) == 1) or ofw.lower() in pNouns):
 		ffw = ''.join(ofl.upper() + iofw)
 	else:
 		ffw = ''.join(ofl.lower() + iofw)
 	return ffw
 
 def setLastCapGlobal(verse):
-	global lc
 	lastchar = verse[len(verse)-1][-1::]
 
-	if (lastchar == '?' or lastchar == '.' or lastchar == '!' or lastchar == ":" or lastchar == ";"):
+	if (lastchar in puncRequireCap):
 		lc = 1
 	else:
 		lc = 0
 
-def getVerses(numverses, freeverse):
+def getSentence(mwps):
+	s = createSentence()
+	while(len(s) < mwps):
+		s = createSentence()
+	return s
+
+def createVerseFromSentence(sentence):
+	verse = sentence
+	verse[0] = modifyFirstChar(verse)
+	setLastCapGlobal(verse)
+	return verse
+
+def getVerses():
 	lastRandomNum = 0
 
-	for _ in range(0, numverses+1):
-		s = createSentence(freeverse)
-		if (len(s) > 2):
-			verse = s
-			verse[0] = modifyFirstChar(verse)
-			setLastCapGlobal(verse)
-			if(len(verse) > 8):
+	for _ in range(0, n+1):
+		s = getSentence(minwps)
+		verse = createVerseFromSentence(s)
+		if(len(verse) > maxwpl):
+			tempstr = ''
+			low = 0
+			top = randint(minwpl, maxwpl)
+			while(low < len(verse)):
 				tempstr = ''
-				low = 0
-				top = randint(5, 8)
-				while(low < len(verse)):
-					tempstr = ''
-					for i in range(low, top):
-						tempstr += verse[i] + ' '
-					low = top
-					newrand = randint(5, 8)
-					top = top + newrand if (top + newrand < len(verse)) else len(verse)
-					writeAndPrint(tempstr.strip(' '))
-			else:
-				writeAndPrint(' '.join(verse).strip())
+				for i in range(low, top):
+					tempstr += verse[i] + ' '
+				low = top
+				newrand = randint(minwpl, maxwpl)
+				top = top + newrand if (top + newrand < len(verse)) else len(verse)
+				addToOutput(tempstr.strip(' '))
+		else:
+			addToOutput(' '.join(verse).strip())
+
+def closeFileIO():
+	f_output.close()
+	f_feed.close()
 
 def program():
-	n = 5
-	fv = 1
-	#n = int(input("Please enter the number of verses you would like: "))
-	#fv = int(input("Please enter '1' for freeverse or '0' for random chance of back-to-back lines: "))
-	getVerses(n, fv)
-	writeAndPrint("\n#PoemsFromAtrus")
-	f_output.close()
-	
-	
+	getVerses()
+	prepareOutput()
+	printOutput()
+	closeFileIO()
 
 program()
