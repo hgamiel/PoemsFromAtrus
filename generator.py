8b9d413ca5e0735dc7f5d6d95692ae864cfc4872
Hannah Gamiel <hannah.gamiel@gmail.com>
2015-10-18 18:39:22 -0700

    Added appropriate comments, fixed upper limit character check for tweets. I forgot to account for the fact that clim already includes the length of the signature, and also forgot to account for verses that need to be split up onto several lines. "i" has been added to the pronouns list to shorten the if statement in modifyFirstChar.

--- generator.py
+++ generator.py
@@ -1,33 +1,40 @@
-# Atrus script
+#  Atrus script
 
 import re
 import random
+import math
 from random import randint
 
-f_output = open('output.txt','w')
-f_feed = open('atrus.txt','r')
-r_feed = f_feed.read()
+# -- BEGIN MODIFIABLE VARIABLES --
 
-punc = ["?", "--", ".", ",", "!", " ", ";", ".", " "] #list of puncation
-puncRequireCap = ["?", ".", "!", ":", ";"] #list of punctuation that requires capitalized letter after
-sentences = list(re.split(r' *[\.\?!][\'"\)\]]* *', r_feed)) #list of sentences
-pNouns = ["catherine", "atrus", "achenar", "sirrus", "d'ni"] #list of names/proper nouns
+u_output = "output.txt" # file used for the output
+u_input = "atrus.txt" # file used for the input (the list of sentences)
+sig = "\n#PoemsFromAtrus" #  signature put at the end of the tweet
+n = 5 # num sentences
+rm = 1 # random modifier
+minwps = 3 # minimum words per sentence
+minwpl = 5 # minimum words per line
+maxwpl = 8 # maximum words per line
+lc = 1 # last cap bool
+li = 0 # last sentence index
+clim = 140 - len(sig) # tweet character limit
+uclim = 1 # whether or not to character limit
 
-oBuffer = []
-oString = ""
-sig = "\n#PoemsFromAtrus"
+#  -- END MODIFIABLE VARIABLES --
 
-n = 5 #num sentences
-rm = 1 #random modifier
-minwps = 3 #minimum words per sentence
-minwpl = 5 #minimum words per line
-maxwpl = 8 #maximum words per line
-lc = 1 #last cap bool
-lastIndex = 0 #last sentence index
-clim = 140 - len(sig) #twitter character limit
-uclim = 1 #whether or not to character limit
+f_output = open(u_output,'w') # output file
+f_feed = open(u_input,'r') # input file
+r_feed = f_feed.read() # read contents of input file
 
-rccnt = 0 #running character count
+punc = ["?", "--", ".", ",", "!", " ", ";", ".", " ", "..."] # list of puncation
+puncRequireCap = ["?", ".", "!", ":", ";"] # list of punctuation that requires capitalized letter after
+sentences = list(re.split(r' *[\.\?!][\'"\)\]]* *', r_feed)) # list of sentences
+pNouns = ["catherine", "atrus", "achenar", "sirrus", "d'ni", "i"] # list of names/proper nouns
+
+oBuffer = [] # output buffer
+oString = "" # final output string
+fcnt = 0 # final character count
+rccnt = 0 # running character count
 
 def addToOutput(segment):
 	global rccnt
@@ -36,22 +43,22 @@ def addToOutput(segment):
 
 def prepareOutput():
 	global oString
-	oString = "\"" + "\n".join(oBuffer) + "\"" + "\n" + sig
+	oString = "\"" + "\n".join(oBuffer).strip(' ') + "\"" + "\n" + sig
 	f_output.write(oString)
 
 def printOutput():
 	print(oString)
 
 def createSentence():
-	global lastIndex
+	global li
 	s = []
-	newIndex = randint(0, len(sentences)-1)
+	ni = randint(0, len(sentences)-1) # new index
 
 	if(rm):
-		while((newIndex == lastIndex) or (newIndex + 1 == lastIndex) or (newIndex - 1 == lastIndex)):
-			newIndex = randint(0, len(sentences)-1)
-		lastIndex = newIndex
-		s = list(sentences[lastIndex].split(' '))
+		while((ni == li) or (ni + 1 == li) or (ni - 1 == li)):
+			ni = randint(0, len(sentences)-1)
+		li = ni
+		s = list(sentences[li].split(' '))
 	else:
 		s = list(random.choice(sentences).split(' '))
 	p = random.choice(punc)
@@ -60,12 +67,12 @@ def createSentence():
 	return s
 
 def modifyFirstChar(verse):
-	ffw = ''; #final first word
-	ofw = verse[0] #original first word of verse
-	ofl = ofw[0] #original first letter of verse
-	iofw =  ofw[1:len(verse[0])] #original first word minus first letter
+	ffw = ''; # final first word
+	ofw = verse[0] # original first word of verse
+	ofl = ofw[0] # original first letter of verse
+	iofw =  ofw[1:len(verse[0])] # original first word minus first letter
 
-	if (lc == 1 or (ofl == "i" and len(ofw) == 1) or ofw.lower() in pNouns):
+	if (lc == 1 or ofw.lower() in pNouns):
 		ffw = ''.join(ofl.upper() + iofw)
 	else:
 		ffw = ''.join(ofl.lower() + iofw)
@@ -94,32 +101,33 @@ def createVerseFromSentence(sentence):
 
 def getVerses():
 	global rccnt
-	lastRandomNum = 0
+	global fcnt
 
 	for _ in range(0, n+1):
 		s = getSentence(minwps)
-		verse = createVerseFromSentence(s)
-		ccount = (rccnt + len(sig) + 2 + len(' '.join(verse)))
-		if(not uclim or (uclim and ccount < clim)):
-			if(len(verse) > maxwpl):
+		v = createVerseFromSentence(s)
+		ml = math.ceil(len(v) / minwpl) if (len(v) > minwpl) else 1 # get max num of lines possible
+		checkmax = (rccnt + 2 + len(' '.join(v).strip(' ')) + ml + len(oBuffer)) # check the current max (+ possible upper limit) num of characters at the moment
+		if(not uclim or (uclim and checkmax < clim)):
+			if(len(v) > maxwpl):
 				tempstr = ''
 				low = 0
 				top = randint(minwpl, maxwpl)
-				while(low < len(verse)):
+				while(low < len(v)):
 					tempstr = ''
 					for i in range(low, top):
-						tempstr += verse[i] + ' '
+						tempstr += v[i] + ' '
 					low = top
 					newrand = randint(minwpl, maxwpl)
-					top = top + newrand if (top + newrand < len(verse)) else len(verse)
+					top = top + newrand if (top + newrand < len(v)) else len(v)
 					addToOutput(tempstr.strip(' '))
 			else:
-				addToOutput(' '.join(verse).strip())
+				addToOutput(' '.join(v).strip())
 		elif(uclim and len(oBuffer) == 0):
 			rccnt = 0
 		elif(uclim):
-			ccount = len("\"" + "\n".join(oBuffer) + "\"" + "\n" + sig)
-			print("Breaking: Character count = " + str(ccount) + "\n")
+			fcnt = len("\"" + "\n".join(oBuffer) + "\"" + "\n" + sig) # final char count
+			print("Breaking: Character count = " + str(fcnt) + "\n")
 			break
 
 def closeFileIO():
